b567624ed6af2d871da15d95900c559155a44f61
Andrei Nastasiu <nastasiuandrei@gmail.com>
2018-02-06 14:12:10 +0200

    Remove dublicated code from extension

--- Sources/KeyboardHandlerForCenteringConstraint.swift
+++ Sources/KeyboardHandlerForCenteringConstraint.swift
@@ -26,44 +26,7 @@ public class KeyboardHandlerForCenteringConstraint: KeyboardHandler, KeyboardSho
             break
         }
     }
-    
-    public func startListeningForShowingOrHidingTheKeyboard() {
-//        let willHideToken = NotificationCenter.default.addObserver(forName: NSNotification.Name.UIKeyboardWillHide, object: nil, queue: OperationQueue.main, using: { (notification) in
-//            self.handleKeyboardNotification(notification, withKeyboardStatus: KeyboardStatus.willHide)
-//        })
-//
-//        let willShowToken = NotificationCenter.default.addObserver(forName: NSNotification.Name.UIKeyboardWillShow, object: nil, queue: OperationQueue.main, using: { (notification) in
-//            self.handleKeyboardNotification(notification, withKeyboardStatus: KeyboardStatus.willShow)
-//        })
-//
-//        let didHideToken = NotificationCenter.default.addObserver(forName: NSNotification.Name.UIKeyboardDidHide, object: nil, queue: OperationQueue.main, using: { (notification) in
-//            self.handleKeyboardNotification(notification, withKeyboardStatus: KeyboardStatus.didHide)
-//        })
-//
-//        let didShowToken = NotificationCenter.default.addObserver(forName: NSNotification.Name.UIKeyboardDidShow, object: nil, queue: OperationQueue.main, using: { (notification) in
-//            self.handleKeyboardNotification(notification, withKeyboardStatus: KeyboardStatus.didShow)
-//        })
-//
-//        if tokens == nil {
-//            tokens = [AnyObject]()
-//        }
-//
-//        tokens?.append(willHideToken)
-//        tokens?.append(willShowToken)
-//        tokens?.append(didHideToken)
-//        tokens?.append(didShowToken)
-    }
-    
-    public func stopListeningForShowingOrHidingKeyboard() {
-//        if let tokens = tokens {
-//            for token in tokens {
-//                NotificationCenter.default.removeObserver(token)
-//            }
-//        }
-//        tokens?.removeAll()
-    }
-    
-    
+        
     private func handleKeyboardNotification(_ notification: Notification, withKeyboardStatus keyboardStatus: KeyboardStatus) {
 //        if let userInfo = (notification as NSNotification).userInfo {
 //            let keyboardRect = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
